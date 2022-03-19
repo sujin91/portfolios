b2697156149bb3b7920f8262927e86125abf7e5f
sujin.shin <soojin3393@gmail.com>
2022-03-20 03:33:15 +0900

    [portfolio] works 추가

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -16,7 +16,7 @@ let render_projects = (slug) => {
             title: 'Clock-App',
             technologies: ['VanillaJS'],
             description: "MVC + 옵저버패턴을 사용한 시계, 알람, 스톱워치 기능을 가진 시계앱입니다. Vanilla JS로만 구현하였습니다.",
-            categories: ['webdev', 'diy']
+            categories: ['fedev', 'diy']
         },
         {
             image: 'assets/my/wetube.png',
@@ -24,7 +24,7 @@ let render_projects = (slug) => {
             title: 'Wetube',
             technologies: ['VanillaJS', 'Express', 'MongoDB'],
             description: "'유튜브 클론코딩', 유저들은 영상을 재생하고, 댓글을 달고, 검색할 수 있으며, 자기 자신의 비디오를 녹화하고 업로드 할 수 있습니다.",
-            categories: ['webdev', 'diy']
+            categories: ['fedev', 'diy']
         },
         {
             image: 'assets/my/nomflix.png',
@@ -32,18 +32,48 @@ let render_projects = (slug) => {
             title: 'Nomflix',
             demo: false,
             technologies: ['ReactJS'],
-            description: "영화 웹서비스 만들기, 리액트를 활용한 간단한 영화 웹 서비스입니다. ",
-            categories: ['webdev', 'diy']
+            description: "영화 웹서비스 만들기, 리액트에서 비동기처리, API통신을 연습하기 위한 간단한 영화 웹 서비스입니다. ",
+            categories: ['fedev', 'diy']
         },
-        // {
-        //     image: 'assets/images/mpw.jpg',
-        //     link: 'https://github.com/abhn/mpw',
-        //     title: 'Master Password',
-        //     demo: 'https://www.nagekar.com/mpw',
-        //     technologies: ['Semantic UI', 'CSS3'],
-        //     description: "Master Password is an ingenious password solution that makes your passwords truly impossible to lose.",
-        //     categories: ['featured', 'security']
-        // },
+        {
+            image: 'assets/my/tokyo2020_2.png',
+            title: '2020 TOKYO',
+            technologies: ['Semantic UI', 'ReactJS', 'CSS'],
+            description: "네이버 2020 도쿄올림픽 특집페이지, React에서 마크업을 개발하였습니다. #다크모드 #반응형웹",
+            categories: ['uidev', 'works']
+        },
+        {
+            image: 'assets/my/election1.png',
+            link: 'https://news.naver.com/main/election/region2018/',
+            title: '제7회 전국동시지방선거',
+            technologies: ['Semantic UI', 'CSS'],
+            description: "네이버 제7회 전국동시지방선거 특집페이지, 마크업을 개발하였습니다.",
+            categories: ['uidev', 'works']
+        },
+        {
+            image: 'assets/my/election5.png',
+            link: 'https://m.news.naver.com/election/nation2020/exit/regional',
+            title: '제21대 국회의원선거',
+            technologies: ['Semantic UI', 'CSS'],
+            description: "네이버 제21대 국회의원선거 특집페이지, 마크업을 개발하였습니다.",
+            categories: ['uidev', 'works']
+        },
+        {
+            image: 'assets/my/shopping.png',
+            link: 'https://campaign.naver.com/2021chart',
+            title: '2021 연말차트',
+            technologies: ['VanillaJS', 'Jquery'],
+            description: "2021년도 쇼핑 내역 총정리! 쇼핑 API를 사용하여 네이버 쇼핑 관련 데이터를 노출합니다.",
+            categories: ['fedev', 'works']
+        },
+        {
+            image: 'assets/my/whaleonstudy.png',
+            link: 'https://campaign.naver.com/whaleonstudy',
+            title: '웨일온스터디',
+            technologies: ['TypeScript'],
+            description: "웨일온스터디 테마좋아요 이벤트, 스터디 테마의 동영상과 컨텐츠를 노출하고 좋아하는 테마를 투표합니다.",
+            categories: ['fedev', 'works']
+        },
         // {
         //     image: 'assets/images/social-share-count.jpeg',
         //     link: 'https://github.com/abhn/Social-Share-Counts',
